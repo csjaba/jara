1d5722ddc287555bf8934b114808063b3372eb95
Alberto Yoshimiya <ayoshimiya@gmail.com>
2011-12-23 11:58:13 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jara::Application.config.secret_token = '6be7adea40f6e38cf96d299e0b27c9273090f3d7f16b060904e02936e48784b02e1afa24a77079d0d78473ee2c24f6eba0a8859f63a6fca52a731686582c2aba'
